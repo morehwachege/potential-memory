655789e5557b7ad9d4dc4527a4a1b7311bbf2572
morehwachege <antony123muriithi@gmail.com>
2022-03-31 04:19:11 +0300

    editor complete

--- public/assets/js/editor.js
+++ public/assets/js/editor.js
@@ -3,17 +3,6 @@ var getText = document.querySelector(".getText");
 var content = document.querySelector(".getcontent");
 var editorContent = document.querySelector(".editor");
 
-btn.addEventListener("click", function() {
-  var s = editorContent.innerHTML;
-  content.style.display = "block";
-  content.textContent = s;
-});
-
-getText.addEventListener("click", function() {
-  const old = editorContent.textContent;
-  content.style.display = "block";
-  content.textContent = old;
-});
 
 function link() {
   var url = prompt("Enter the URL");
@@ -55,11 +44,11 @@ function getImage() {
   }
 }
 
-function printMe() {
-  if (confirm("Check your Content before print")) {
+function printArticle() {
+  if (confirm("Confirm the items you want to print in the editor section. \nClick OK to print")) {
     const body = document.body;
-    let s = body.innerHTML;
-    body.textContent = editorContent.innerHTML;
+    let s = body.textContent;
+    body.textContent = editorContent.textContent;
 
     document.execCommandShowHelp;
     body.style.whiteSpace = "pre";
